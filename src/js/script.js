777638ed25145779e03519f37a1fba678e3da035
Fadhil Rifqi Rabbani Pane <163860803+Belligool@users.noreply.github.com>
2025-09-25 09:52:38 +0700

    added description for course cards

--- src/js/script.js
+++ src/js/script.js
@@ -5,13 +5,13 @@ const subjects = {
       line: "Mathematics",
       subline: "Featuring Killjoy",
       lessons: [
-        { title: "Functions", url: "https://www.youtube.com/watch?v=5D5b3c1DjZw", desc: "bla bla bla" },
-        { title: "Limits", url: "https://www.youtube.com/watch?v=YNstP0ESndU", desc: "bla bla bla"},
-        { title: "Derivatives", url: "https://www.youtube.com/watch?v=5yfh5cf4-0w", desc: "bla bla bla" },
-        { title: "Application of Derivatives", url: "https://www.youtube.com/watch?v=WiOdQQYLMU4", desc: "bla bla bla"},
-        { title: "Integrals", url: "https://www.youtube.com/watch?v=rfG8ce4nNh0", desc: "bla bla bla" },
-        { title: "Application of Integrals", url: "https://www.youtube.com/watch?v=H71xfyzbkZI", desc: "bla bla bla"},
-        { title: "Transcendants Functions", url: "https://www.youtube.com/watch?v=F_u6vAWmWl0", desc: "bla bla bla" }
+        { title: "Functions", url: "https://www.youtube.com/watch?v=5D5b3c1DjZw", desc: "Learn how function works!" },
+        { title: "Limits", url: "https://www.youtube.com/watch?v=YNstP0ESndU", desc: "Learn how limits works!"},
+        { title: "Derivatives", url: "https://www.youtube.com/watch?v=5yfh5cf4-0w", desc: "Learn about Derivatives!" },
+        { title: "Application of Derivatives", url: "https://www.youtube.com/watch?v=WiOdQQYLMU4", desc: "Learn how Derivatives is applied!"},
+        { title: "Integrals", url: "https://www.youtube.com/watch?v=rfG8ce4nNh0", desc: "Learn about Integrals!" },
+        { title: "Application of Integrals", url: "https://www.youtube.com/watch?v=H71xfyzbkZI", desc: "Learn about the application of integrals!"},
+        { title: "Transcendants Functions", url: "https://www.youtube.com/watch?v=F_u6vAWmWl0", desc: "Learn about transcendant functions!" }
       ]
     },
     CHEMISTRY: {
@@ -20,13 +20,13 @@ const subjects = {
       line: "General Chemistry",
       subline: "Featuring Viper",
       lessons: [
-        { title: "Elements, Molecules and The Periodic Table", url: "https://www.youtube.com/watch?v=TXEXWN8J8BE", desc: "bla bla bla" },
-        { title: "The Concept of Moles and Stoichiometry", url: "https://www.youtube.com/watch?v=UPhIEtYKjd8", desc: "bla bla bla" },
-        { title: "Energy and It's Transformations", url: "https://www.youtube.com/watch?v=PKm4ZVNmJyQ", desc: "bla bla bla"},
-        { title: "Atomic Structure", url: "https://www.youtube.com/watch?v=OH-aSu-rWgk", desc: "bla bla bla" },
-        { title: "Chemical Bonds and Molecular Structure", url: "https://www.youtube.com/watch?v=H1-COuLbvzI", desc: "bla bla bla" },
-        { title: "Intermolecular Forces and Properties of Liquids", url: "https://www.youtube.com/watch?v=ubLcR2un-Ao", desc: "bla bla bla" },
-        { title: "Solids", url: "https://www.youtube.com/watch?v=Z57pyi7Y5lg", desc: "bla bla bla" }
+        { title: "Elements, Molecules and The Periodic Table", url: "https://www.youtube.com/watch?v=TXEXWN8J8BE", desc: "Learn all about the periodic table!" },
+        { title: "The Concept of Moles and Stoichiometry", url: "https://www.youtube.com/watch?v=UPhIEtYKjd8", desc: "Learn about moles and stoichiometry!" },
+        { title: "Energy and It's Transformations", url: "https://www.youtube.com/watch?v=PKm4ZVNmJyQ", desc: "Learn about energies and how it transforms!"},
+        { title: "Atomic Structure", url: "https://www.youtube.com/watch?v=OH-aSu-rWgk", desc: "Learn about the structures of atoms!" },
+        { title: "Chemical Bonds and Molecular Structure", url: "https://www.youtube.com/watch?v=H1-COuLbvzI", desc: "Learn about chemical bonds!" },
+        { title: "Intermolecular Forces and Properties of Liquids", url: "https://www.youtube.com/watch?v=ubLcR2un-Ao", desc: "Learn various things about intermolecular forces!" },
+        { title: "Solids", url: "https://www.youtube.com/watch?v=Z57pyi7Y5lg", desc: "Learn about solids in chemistry!" }
       ]
     },
     ENGLISH: { 
@@ -35,11 +35,11 @@ const subjects = {
         line: "English Language",
         subline: "Featuring Luna Snow",
         lessons: [
-          { title: "Past Tense, Present Tense, Future Tense", url: "https://www.youtube.com/watch?v=ld7RqdoRAM0", desc: "bla bla bla" },
-          { title: "Grammar", url: "https://www.youtube.com/watch?v=QXVzmzhxWWc", desc: "bla bla bla" },
-          { title: "Vocabulary", url: "https://www.youtube.com/watch?v=u1QVJ77GGgg", desc: "bla bla bla" },
-          { title: "Pronunciation", url: "https://www.youtube.com/watch?v=NgwlinpopRQ", desc: "bla bla bla" },
-          { title: "Advanced English", url: "https://www.youtube.com/watch?v=Iv4F-YV0TxY", desc: "bla bla bla"}
+          { title: "Past Tense, Present Tense, Future Tense", url: "https://www.youtube.com/watch?v=ld7RqdoRAM0", desc: "Learn about the various tenses in the English language!" },
+          { title: "Grammar", url: "https://www.youtube.com/watch?v=QXVzmzhxWWc", desc: "Learn about how to use grammars correctly!" },
+          { title: "Vocabulary", url: "https://www.youtube.com/watch?v=u1QVJ77GGgg", desc: "Learn how to build your vocabulary!" },
+          { title: "Pronunciation", url: "https://www.youtube.com/watch?v=NgwlinpopRQ", desc: "Learn how to pronounce some words!" },
+          { title: "Advanced English", url: "https://www.youtube.com/watch?v=Iv4F-YV0TxY", desc: "Learn about advanced English!"}
         ]
       },
     SPORTS: {
@@ -48,11 +48,11 @@ const subjects = {
       line: "Sports",
       subline: "Featuring Magik",
       lessons: [
-        { title: "Warm-up and Stretching Basics", url: "https://www.youtube.com/watch?v=2L2lnxIcNmo", desc: "bla bla bla" },
-        { title: "Cardio Training 101", url: "https://www.youtube.com/watch?v=ml6cT4AZdqI", desc: "bla bla bla" },
-        { title: "Strength Training for Beginners", url: "https://www.youtube.com/watch?v=xO3NJ7A7w5o", desc: "bla bla bla" },
-        { title: "Study Case: Manchester United", url: "https://youtu.be/Dki5tjN0rDE?si=slRvri_8aGfygXBs", desc: "bla bla bla" },
-        { title: "Football Tactics", url: "https://www.youtube.com/watch?v=4wAG5ZUJ4eI", desc: "bla bla bla"}
+        { title: "Warm-up and Stretching Basics", url: "https://www.youtube.com/watch?v=2L2lnxIcNmo", desc: "Learn the basics before you workout!" },
+        { title: "Cardio Training 101", url: "https://www.youtube.com/watch?v=ml6cT4AZdqI", desc: "Learn how to properly train cardio!" },
+        { title: "Strength Training for Beginners", url: "https://www.youtube.com/watch?v=xO3NJ7A7w5o", desc: "Learn how to up your strength!" },
+        { title: "Study Case: Manchester United", url: "https://youtu.be/Dki5tjN0rDE?si=slRvri_8aGfygXBs", desc: "Learn how NOT to play football so you don't make the same mistake!" },
+        { title: "Football Tactics", url: "https://www.youtube.com/watch?v=4wAG5ZUJ4eI", desc: "Learn from The Special One!"}
       ]
     },
     AI: {
@@ -61,15 +61,15 @@ const subjects = {
         line: "Artificial Intelligence & Data Literacy",
         subline: "Featuring Ultron",
         lessons: [
-          { title: "Types of Data and Sampling Methods", url: "https://www.youtube.com/watch?v=jk68oU9nu5E", desc: "bla bla bla" },
-          { title: "Exploratory Data Analysis (EDA)", url: "https://www.youtube.com/watch?v=Liv6eeb1VfE", desc: "bla bla bla" },
-          { title: "Univariate and Bivariate Data Analysis", url: "https://www.youtube.com/watch?v=g5OmksmzaWI", desc: "bla bla bla" },
-          { title: "Probability and Bayes' Theorem", url: "https://www.youtube.com/watch?v=XQoLVl31ZfQ", desc: "bla bla bla" },
-          { title: "Definition of Artificial Intelligence (AI)", url: "https://www.youtube.com/watch?v=JcXKbUIebrU", desc: "bla bla bla" },
-          { title: "Intelligent Agent Levels and Types of Machine Learning", url: "https://www.youtube.com/watch?v=fXizBc03D7E", desc: "bla bla bla" },
-          { title: "Supervised Machine Learning", url: "https://www.youtube.com/watch?v=Mu3POlNoLdc", desc: "bla bla bla" },
-          { title: "Generative AI Large Language Model (LLM)", url: "https://www.youtube.com/watch?v=AneKPA_0b0s", desc: "bla bla bla" },
-          { title: "AI Ethics and Case Studies", url: "https://www.youtube.com/watch?v=YT-lPqYbv9k", desc: "bla bla bla" }
+          { title: "Types of Data and Sampling Methods", url: "https://www.youtube.com/watch?v=jk68oU9nu5E", desc: "Learn about the various data types!" },
+          { title: "Exploratory Data Analysis (EDA)", url: "https://www.youtube.com/watch?v=Liv6eeb1VfE", desc: "Learn about advanced data analysis!" },
+          { title: "Univariate and Bivariate Data Analysis", url: "https://www.youtube.com/watch?v=g5OmksmzaWI", desc: "Learn about the two types of data analysis!" },
+          { title: "Probability and Bayes' Theorem", url: "https://www.youtube.com/watch?v=XQoLVl31ZfQ", desc: "Learn about probability!" },
+          { title: "Definition of Artificial Intelligence (AI)", url: "https://www.youtube.com/watch?v=JcXKbUIebrU", desc: "Learn exactly what an AI is!" },
+          { title: "Intelligent Agent Levels and Types of Machine Learning", url: "https://www.youtube.com/watch?v=fXizBc03D7E", desc: "Learn how an AI learn!" },
+          { title: "Supervised Machine Learning", url: "https://www.youtube.com/watch?v=Mu3POlNoLdc", desc: "Learn how an AI study under supervision!" },
+          { title: "Generative AI Large Language Model (LLM)", url: "https://www.youtube.com/watch?v=AneKPA_0b0s", desc: "Learn about generative AI!" },
+          { title: "AI Ethics and Case Studies", url: "https://www.youtube.com/watch?v=YT-lPqYbv9k", desc: "Learn about the ethics of AI usage!" }
         ]
       }
   };
